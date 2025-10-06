268da09cfbf16e4ed101e5b440d1f42608339749
Whombit <122506391+Whombit@users.noreply.github.com>
2025-10-05 23:20:06 -0400

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <windows.h>  // For Sleep() on Windows
+#include <windows.h>
 
 int main() {
     char alphabet[34] = " abcdefghijklmnopqrstuvwxyz.!?():;";
@@ -21,11 +21,11 @@ int main() {
     for (int i = 0; i < strlen(word); i++) {
         char c = tolower(word[i]);
 
-        for (int j = 0; j < 34; j++) {  // Loop should go up to 34 now
+        for (int j = 0; j < 34; j++) {
             printf("%s ", completed);
             printf("%c\n", alphabet[j]);
             fflush(stdout);
-            Sleep(50);  // Sleep in milliseconds
+            Sleep(50);
 
             if (alphabet[j] == c) {
                 strncat(completed, &c, 1);
